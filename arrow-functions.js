7221c0e41228b20bf37c8377896b1b49fd8f2c88
bear81 <alongcameabear@gmail.com>
2024-03-07 09:14:21 +0000

    Arrow Functions + practice problem

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,36 @@
  */
 
 // Vanilla JavaScript Function
+function addTwoNumbers(a, b) {
+    //code Bloack
+    return a + b;
+}
+let sum = addTwoNumbers(3,5);
+console.log(sum)
 
 // Arrow Function With Parameters
+const addTwoNumbersV2 = (a, b) => {
+    //code Bloack
+    return a + b;
+}
+let sumTwo = addTwoNumbersV2(4, 6);
+console.log(sumTwo)
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbersV3 = (a, b) => a + b;
+let sumThree = addTwoNumbersV3(5,7);
+console.log(sumThree);
 
 // Implicit Returns
+const saySomething = message => console.log(message)
+saySomething("This is my message");
+
+const saySomethingElse = () => console.log("This is something else")
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+      This is a multiline string!
+    </p>`
+)
+console.log(returnMultipleLines());
---
+++ gemini-problem-1.js
@@ -0,0 +1,15 @@
+/* Tasks:
+
+Calculate Total Duration:  Write a function named totalDuration that takes the playlist array as input and returns the total duration of all songs in seconds.
+
+Format Duration (Optional):  Create a function named formatDuration that takes a duration in seconds and returns a string formatted as "minutes:seconds" (e.g., 235 seconds becomes "3:55").
+*/
+
+
+const playlist = [
+    { title: "Don't Stop Me Now", artist: "Queen", duration: 235 }, // Duration in seconds
+    { title: "Hey Jude", artist: "The Beatles", duration: 431 },
+    { title: "Smells Like Teen Spirit", artist: "Nirvana", duration: 250 },
+    { title: "Bohemian Rhapsody", artist: "Queen", duration: 354 },
+    { title: "Imagine", artist: "John Lennon", duration: 183 }
+];
